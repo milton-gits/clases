83a2b3449bdb0ff3221e905f3ef79c5e3ed997db
milton <milton8686@hotmail.com>
2023-03-05 11:02:07 -0300

    Este es mi primer commit

---
+++ Clase1/src/main/java/com/mycompany/clase1/Clase1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.clase1;
+
+/**
+ *
+ * @author Death
+ */
+public class Clase1 {
+
+    public static void main(String[] args) {
+
+        int num1 = 1;
+        int num2 = 20;
+//Primer punto:
+
+         /*while (num1 <= num2) {
+            System.out.println("" + num1);
+            num1++;
+        }*/
+         
+ //Segundo punto:
+    
+       /* while (num1 < num2) {
+            num1++;
+            if (num1 % 2 == 0) {
+                System.out.println("" + num1);
+            }
+
+        }*/
+        
+        for(int i=10;i>num1;i--){
+            System.out.println(""+num2);
+            num2=num2-2;
+          
+            
+        }
+    }
+}
