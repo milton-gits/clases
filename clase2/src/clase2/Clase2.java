d150928b46c9fa57998082a7f64534204f993899
milton <milton8686@hotmail.com>
2023-03-12 17:39:47 -0300

    entrando a github

--- clase2/src/clase2/Clase2.java
+++ clase2/src/clase2/Clase2.java
@@ -14,7 +14,7 @@ public class Clase2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("chau mundo");
     }
    
 }
